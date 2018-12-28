104f2401f0a20bce5245b10314b9a1c2ff584abb
m-fiks <fiksd005@umn.edu>
2018-12-27 22:55:03 -0600

    countdown working

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const date = Date.now();
 let startTime = 25;
+let startSecond = 60;
 
 // appendage = () => {
 //     let poppy = document.getElementById("timer")
@@ -12,14 +13,17 @@ const start = document.getElementById("start-button")
 
 minusAMinute = () => {
     startTime--;
-    document.getElementById("timer").innerHTML = startTime;
-    console.log(startTime)
+    document.getElementById("timer").innerHTML = `${startTime}:${startSecond}`;
+}
+
+minusSecond = () => {
+    startSecond--;
+    document.getElementById("timer").innerHTML = `${startTime}:${startSecond}`;
 }
 
 start.addEventListener('click', () => {
-    document.getElementById("timer").innerHTML = startTime;
     setInterval(minusAMinute, 60000);
+    setInterval(minusSecond, 1000);
 })
-
 
 
