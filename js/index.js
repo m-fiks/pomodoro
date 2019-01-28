06baa596dca2ee2a119924b323b50cabcdb3646c
m-fiks <fiksd005@umn.edu>
2019-01-27 22:08:26 -0600

    bed

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const date = Date.now();
 let startTime = 24;
 let startSecond = 60;
-let paused = false;
+let paused;
 const start = document.getElementById("start-button");
 const pause = document.getElementById("pause-button");
 let breakTime = 5;
@@ -10,27 +10,18 @@ let breakTime = 5;
 console.log(moment().format('dddd'))
 
 function minusAMinute() {
-    if (paused) {
-        document.getElementById("timer").innerHTML = `PAWSEDDD`
-    }
-    else {
-        setInterval(function() {
-            if (startTime === 0) {
-                //start break time
-            }
-            else {
-                startTime--;
-            }
-            document.getElementById("timer").innerHTML = `${startTime}:${startSecond}`;
-        }, 60000);
-}   ;
+    setInterval(function() {
+        if (startTime === 0) {
+            //start break time
+        }
+        else {
+            startTime--;
+        }
+        document.getElementById("timer").innerHTML = `${startTime}:${startSecond}`;
+    }, 60000);
 };
 
 function minusASecond () {
-    if (paused) {
-        console.log('paws')
-    }
-    else {
     setInterval(function() {
             if (startSecond === 1) {
                 startSecond = 60;
@@ -38,21 +29,26 @@ function minusASecond () {
             startSecond--;
             document.getElementById("timer").innerHTML = `${startTime}:${startSecond < 10 ? '0' : ''}${startSecond}`;
         }, 1000);
-    }
 }
 
 start.addEventListener('click', () => {
+    paused = false;
     minusAMinute();
     minusASecond();
     pauseTimer();
     document.getElementById("timer").innerHTML = `${startTime}:${startSecond}`;
 })
 
-pauseTimer = () => {
+function pauseTimer () {
     pause.addEventListener("click", () => {
-        paused = true;
-        minusAMinute();
-        minusASecond();
-        console.log(paused)
+        console.log('psfdsk')
+        onPause();
     })
+}
+
+function onPause () {
+    paused = true;
+    clearInterval(minusASecond)
+    clearInterval(minusAMinute)
+    document.getElementById("timer").innerHTML = `paused`
 }
