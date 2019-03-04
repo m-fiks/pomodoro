95ad0ead2aa342bf19f8eb5583ba85cbbb0efa08
m-fiks <fiksd005@umn.edu>
2019-03-04 10:30:18 -0600

    breaktime

--- js/index.js
+++ js/index.js
@@ -4,14 +4,17 @@ const resume = document.getElementById("resume-button");
 const breakTime = document.getElementById("break");
 
 let Clock = {
-    totalSeconds: 1500,
+    totalSeconds: 10,
 
     start: function () {
         let self = this;
 
         this.interval = setInterval (() => {
+            if(self.totalSeconds === 0) {
+                console.log('breaktime');
+                self.totalSeconds = 300;
+            }
             self.totalSeconds -= 1;
-
             $("#min").text(Math.floor(self.totalSeconds / 60));
             $("#sec").text(parseInt(self.totalSeconds % 60));
         }, 1000);
@@ -25,18 +28,6 @@ let Clock = {
     resume: function () {
         if (!this.interval) this.start();
     },
-
-    break: function () {
-        //5 min break
-        let self = this;
-
-        this.interval = setInterval(() => {
-            self.totalSeconds -= 1;
-
-            $("#min").text((Math.floor(self.totalSeconds / 60) -20));
-            $("#sec").text(parseInt(self.totalSeconds % 60));
-        },1000);
-    }
 };
 
 start.addEventListener('click', () => {
@@ -49,8 +40,4 @@ pause.addEventListener('click', () => {
 
 resume.addEventListener('click', () => {
     Clock.resume();
-})
-
-breakTime.addEventListener('click', () => {
-    Clock.break();
 })
